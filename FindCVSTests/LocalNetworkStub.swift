143ca3a4f4782721a5feeb9c40a96b56de378eba
Jay Lee <73763976+jayyaj1224@users.noreply.github.com>
2023-07-13 12:23:26 +0100

    Git Fix

---
+++ FindCVSTests/LocalNetworkStub.swift
@@ -0,0 +1,18 @@
+//
+//  LocalNetworkStub.swift
+//  FindCVSTests
+//
+//  Created by Jay Lee on 30/04/2023.
+//
+
+import Foundation
+import RxSwift
+import Stubber
+
+@testable import FindCVS
+
+class LocalNetworkStub: LocalNetwork {
+    override func getLocation(by mapPoint: MTMapPoint) -> Single<Result<LocationData, URLError>> {
+        return Stubber.invoke(getLocation, args: mapPoint)
+    }
+}
